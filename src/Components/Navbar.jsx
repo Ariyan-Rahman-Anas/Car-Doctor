7e9bf4e8233bcefb02ed434636ea75d54d0b9e2e
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-01-03 22:32:26 +0600

    Finished project initail set up

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,56 @@
+import { Link, NavLink } from "react-router-dom";
+import Logo from "./../assets/logo.svg"
+
+const Navbar = () => {
+    return (
+      <div>
+        <nav className="flex items-center justify-between p-2">
+          <div className="nav-start w-[5rem] md:w-[6rem] ">
+            <Link to={"/"}>
+              <img src={Logo} alt="The Website Logo" className="w-full" />
+            </Link>
+          </div>
+          <div className="navbar-middle hidden md:block">
+            <ul className="flex space-x-8">
+              <li className="tex-white relative group">
+                <NavLink to={"/"} className="group-hover:text-[#ff3811]">
+                  Home
+                  <span className="absolute left-0 right-0 bottom-0 top-[1.35rem] h-[.14rem] w-full rounded-md bg-[#ff3811] transform scale-x-0 origin-bottom transition-transform group-hover:scale-x-100 duration-300 "></span>
+                </NavLink>
+              </li>
+              <li className="tex-white relative group">
+                <NavLink to={"/"} className="group-hover:text-[#ff3811]">
+                  About
+                  <span className="absolute left-0 right-0 bottom-0 top-[1.35rem] h-[.14rem] w-full rounded-md bg-[#ff3811] transform scale-x-0 origin-bottom transition-transform group-hover:scale-x-100 duration-300"></span>
+                </NavLink>
+              </li>
+              <li className="tex-white relative group">
+                <NavLink to={"/"} className="group-hover:text-[#ff3811]">
+                  Services
+                  <span className="absolute left-0 right-0 bottom-0 top-[1.35rem] h-[.14rem] w-full rounded-md bg-[#ff3811] transform scale-x-0 origin-bottom transition-transform group-hover:scale-x-100 duration-300"></span>
+                </NavLink>
+              </li>
+              <li className="tex-white relative group">
+                <NavLink to={"/"} className="group-hover:text-[#ff3811]">
+                  Blog
+                  <span className="absolute left-0 right-0 bottom-0 top-[1.35rem] h-[.14rem] w-full rounded-md bg-[#ff3811] transform scale-x-0 origin-bottom transition-transform group-hover:scale-x-100 duration-300"></span>
+                </NavLink>
+              </li>
+              <li className="tex-white relative group">
+                <NavLink to={"/"} className="group-hover:text-[#ff3811]">
+                  Contact
+                  <span className="absolute left-0 right-0 bottom-0 top-[1.35rem] h-[.14rem] w-full rounded-md bg-[#ff3811] transform scale-x-0 origin-bottom transition-transform group-hover:scale-x-100 duration-300"></span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-end">
+            <Link to={"/"} className="px-5 py-2 border-[.09rem] border-[#ff3811] font-semibold rounded-md text-[#ff3811] hv
+            hover:bg-[#ff3811] hover:text-white duration-500 ">Appointment</Link>
+          </div>
+        </nav>
+      </div>
+    );
+};
+
+export default Navbar;
