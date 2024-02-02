009dd69a55a8c598f5ce41e5b7199d708d3bd2fd
Ariyan Rahman Anas <mohammadariyanrahmananas@gmail.com>
2024-02-02 08:13:50 +0600

    Implemented the sweet alert-2 with a beautiful alert

--- src/All Pages/Checkout/Checkout.jsx
+++ src/All Pages/Checkout/Checkout.jsx
@@ -3,11 +3,60 @@ import PageShortBanner from "../../Components/PageShortBanner";
 import imgBG from "./../../assets/images/checkout/checkout.png"
 import { useContext } from "react";
 import { AuthContext } from './../../Provider/AuthProvider';
+import { useForm } from "react-hook-form";
+import { PiWarningCircle } from "react-icons/pi";
+import Swal from "sweetalert2";
 
 const Checkout = () => {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
+      },
+      buttonsStyling: true,
+    });
+
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => {
+        // swl2
+        swalWithBootstrapButtons
+          .fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, Confirm it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true,
+          })
+          .then((result) => {
+              if (result.isConfirmed) {
+                console.log(data);
+              swalWithBootstrapButtons.fire({
+                title: "Confirmed!",
+                text: "Your order has been accepted. We will contact you soon.",
+                icon: "success",
+              });
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire({
+                title: "Cancelled",
+                text: "You are Canceling the order :)",
+                icon: "error",
+              });
+            }
+          });
+        // swl2
+    }
     const {user} = useContext(AuthContext)
     const service = useLoaderData()
-    const { name } = service
+    const { name, price } = service
 
     return (
       <div className="px-2">
@@ -18,38 +67,103 @@ const Checkout = () => {
           serviceTitle={name}
         ></PageShortBanner>
         <div className="bg-gray-200 lg:w-[85vw] mx-auto p-4 md:p-16 my-16 rounded-md ">
-          <form className="w-full">
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full ">
             <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6">
-              <input
-                type="text"
-                name="first-name"
-                placeholder="First Name:"
-                className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
-              />
-              <input
-                type="text"
-                name="last-name"
-                placeholder="Last Name:"
-                className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
-              />
-              <input
-                type="number"
-                name="phone"
-                placeholder="Phone:"
-                className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
-              />
-              <input
-                type="email"
-                name="email"
-                placeholder="Example@e-mail.com"
-                defaultValue={user ? user.email : ""}
-                className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
-              />
+              <div className="flex flex-col gap-1.5">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="First Name:"
+                  defaultValue={user?.displayName}
+                  {...register("name", { required: true })}
+                  className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
+                />
+                {errors.name && (
+                  <div className="flex items-center justify-start gap-1 text-[#ff3811]">
+                    <PiWarningCircle className="text-2xl"></PiWarningCircle>
+                    <span className="font-normal text-sm">Name required</span>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Example@e-mail.com"
+                  defaultValue={user?.email}
+                  {...register("email", { required: true })}
+                  className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
+                />
+                {errors.email && (
+                  <div className="flex items-center justify-start gap-1 text-[#ff3811]">
+                    <PiWarningCircle className="text-2xl"></PiWarningCircle>
+                    <span className="font-normal text-sm">Email required</span>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <input
+                  type="number"
+                  name="phone"
+                  {...register("phone", { required: true })}
+                  placeholder="Phone:"
+                  className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
+                />
+                {errors.phone && (
+                  <div className="flex items-center justify-start gap-1 text-[#ff3811]">
+                    <PiWarningCircle className="text-2xl"></PiWarningCircle>
+                    <span className="font-normal text-sm">Phone required</span>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <input
+                  type="text"
+                  name="address"
+                  {...register("address", { required: true })}
+                  placeholder="Address:"
+                  className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
+                />
+                {errors.address && (
+                  <div className="flex items-center justify-start gap-1 text-[#ff3811]">
+                    <PiWarningCircle className="text-2xl"></PiWarningCircle>
+                    <span className="font-normal text-sm">
+                      Address required
+                    </span>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <input
+                  type="date"
+                  name="date"
+                  {...register("date", { required: true })}
+                  placeholder="Choose the Date:"
+                  className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
+                />
+                {errors.date && (
+                  <div className="flex items-center justify-start gap-1 text-[#ff3811]">
+                    <PiWarningCircle className="text-2xl"></PiWarningCircle>
+                    <span className="font-normal text-sm">Date required</span>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <input
+                  type="number"
+                  name="price"
+                  placeholder="Price:"
+                  defaultValue={price}
+                  readOnly
+                  className="p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
+                />
+              </div>
             </div>
             <textarea
               name="message"
               cols="30"
               rows="5"
+              {...register("message")}
               placeholder="Your message..."
               className="w-full mt-6 p-2 rounded-md border-y-[.20rem] border-transparent focus:border-b-[#ff3811] focus:outline-none "
             ></textarea>
